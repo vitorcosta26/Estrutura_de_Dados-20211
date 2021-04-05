71a2ef4eb0861d365a3c189884e949d270e8c2b9
vitorcosta26 <60762180+vitorcosta26@users.noreply.github.com>
2021-04-05 10:21:40 -0300

    Add files via upload

---
+++ Lista 3 - Lista Duplamente Encadeada/src/lista_duplamente_encadeada/CelulaDupla.java
@@ -0,0 +1,47 @@
+package lista_duplamente_encadeada;
+
+public class CelulaDupla {
+	private CelulaDupla anterior;
+	private CelulaDupla proxima;
+	private Object elemento;
+	
+	public CelulaDupla() {
+		super();
+	}
+
+	public CelulaDupla(Object elemento) {
+		super();
+		this.elemento = elemento;
+	}
+
+	public CelulaDupla(CelulaDupla anterior, CelulaDupla proxima, Object elemento) {
+		super();
+		this.anterior = anterior;
+		this.proxima = proxima;
+		this.elemento = elemento;
+	}
+
+	public CelulaDupla getAnterior() {
+		return anterior;
+	}
+
+	public void setAnterior(CelulaDupla anterior) {
+		this.anterior = anterior;
+	}
+
+	public CelulaDupla getProxima() {
+		return proxima;
+	}
+
+	public void setProxima(CelulaDupla proxima) {
+		this.proxima = proxima;
+	}
+
+	public Object getElemento() {
+		return elemento;
+	}
+
+	public void setElemento(Object elemento) {
+		this.elemento = elemento;
+	}
+}
