21ae2decba53d8b15ebfbf97a5762ca2517e3196
vitorcosta26 <60762180+vitorcosta26@users.noreply.github.com>
2021-04-29 20:33:03 -0300

    Update Main.java

--- Lista 6 - Deques/src/deque/Main.java
+++ Lista 6 - Deques/src/deque/Main.java
@@ -4,53 +4,69 @@ public class Main {
 
 	public static void main(String[] args) {
 		Deque<Pessoa> deque = new Deque<>();
-		System.out.println(deque);
-        System.out.println(deque.tamanho());
-        System.out.println(deque.isEmpty());
-        
-        Pessoa p1 = new Pessoa("Aa");
-        Pessoa p2 = new Pessoa("Bb");
-        Pessoa p3 = new Pessoa("Cc");
-        Pessoa p4 = new Pessoa("DD");
-        Pessoa p5 = new Pessoa("Ee");
-        Pessoa p6 = new Pessoa("Ff");
-        Pessoa p7 = new Pessoa("Gg");
-        
-        System.out.println();
-        deque.InserirNoInicio(p3);
-        deque.InserirNoFim(p4);
-        System.out.println(deque);
-        System.out.println(deque.tamanho());
-        deque.InserirNoInicio(p2);
-        deque.InserirNoFim(p5);
-        System.out.println(deque);
-        System.out.println(deque.tamanho());
-        deque.InserirNoInicio(p1);
-        System.out.println(deque);
-        System.out.println(deque.tamanho());
-        System.out.println(deque.isEmpty());
-        
-        System.out.println();
-        System.out.println(deque.existeDado(p6));
-        System.out.println(deque.existeDado(p1));
-        System.out.println(deque.existeDado(p3));
-        
-        System.out.println();
-        System.out.println(deque.RecuperarInicio());
-        System.out.println(deque.RecuperarFim());
-        
-        System.out.println();
-        deque.AlterarInicio(p6);
-        deque.AlterarFim(p7);
-        System.out.println(deque);
-        
-        System.out.println();
-        deque.RemoverInicio();
-        deque.RemoverFim();
-        System.out.println(deque);
-        
-        System.out.println();
-        deque.limpa();
-        System.out.println(deque);
+		System.out.println("Fila.\n"+deque);
+		System.out.println("Tamnho: "+deque.tamanho());
+		System.out.println("Vazia: "+deque.isEmpty());
+		
+		Pessoa p1 = new Pessoa("Aa");
+		Pessoa p2 = new Pessoa("Bb");
+		Pessoa p3 = new Pessoa("Cc");
+		Pessoa p4 = new Pessoa("DD");
+		Pessoa p5 = new Pessoa("Ee");
+		Pessoa p6 = new Pessoa("Ff");
+		Pessoa p7 = new Pessoa("Gg");
+		
+		System.out.println();
+		System.out.println("Adiciona Início.");
+        	deque.InserirNoInicio(p3);
+        	System.out.println(deque);
+        	System.out.println("Adiciona Fim.");
+        	deque.InserirNoFim(p4);
+        	System.out.println(deque);
+        	System.out.println("Tamnho: "+deque.tamanho());
+        	System.out.println("Adiciona Início.");
+        	deque.InserirNoInicio(p2);
+        	System.out.println(deque);
+        	System.out.println("Adiciona Fim.");
+        	deque.InserirNoFim(p5);
+        	System.out.println(deque);
+        	System.out.println("Tamnho: "+deque.tamanho());
+        	System.out.println("Adiciona Início.");
+        	deque.InserirNoInicio(p1);
+        	System.out.println(deque);
+        	System.out.println("Tamnho: "+deque.tamanho());
+        	System.out.println("Vazia: "+deque.isEmpty());
+		
+		System.out.println();
+        	System.out.println("Existe \"Ff\": "+deque.existeDado(p6));
+        	System.out.println("Existe \"Aa\": "+deque.existeDado(p1));
+        	System.out.println("Existe \"Cc\": "+deque.existeDado(p3));
+		
+		System.out.println();
+        	System.out.println("Recupera Início. "+deque.RecuperarInicio());
+        	System.out.println("Recupera Fim. "+deque.RecuperarFim());
+		
+		System.out.println();
+        	System.out.println("Alterar Início.");
+        	deque.AlterarInicio(p6);
+        	System.out.println(deque);
+        	System.out.println("Alterar Fim.");
+        	deque.AlterarFim(p7);
+        	System.out.println(deque);
+		
+		System.out.println();
+        	System.out.println("Remove Início.");
+        	deque.RemoverInicio();
+        	System.out.println(deque);
+        	System.out.println("Remove Fim.");
+        	deque.RemoverFim();
+        	System.out.println(deque);
+		
+		System.out.println();
+        	System.out.println("Método Limpa.");
+        	deque.limpa();
+        	System.out.println(deque);
+        	System.out.println("Tamnho: "+deque.tamanho());
+        	System.out.println("Vazia: "+deque.isEmpty());
 	}
 }
